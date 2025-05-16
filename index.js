0f7a0afc4331e733db880ebcfc3aabb2a14cf276
mgwheeler <gallant.wheeler@gmail.com>
2025-05-16 09:38:45 -0700

    feat: updating default port to 8080

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const inventory = require("./inventory.json");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
